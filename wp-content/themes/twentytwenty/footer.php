83249c36b458b943d93e5271dcb2653dff0196c1
giridharuniverse21 <97376524+giridharuniverse21@users.noreply.github.com>
2022-03-03 11:29:49 +0530

    New Commit

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -30,7 +30,7 @@
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress GK CI/CD ', 'twentytwenty' ); ?>
+								<?php _e( 'Powered by WordPress GK CI/CD Works ', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-wordpress -->
 
